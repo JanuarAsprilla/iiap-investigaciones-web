5f8fda1a889844c8ca88170a9a1b3e4a518d14f7
JanuarAsprilla <ajanuarandres@gmail.com>
2026-05-29 10:17:46 -0500

    fix: hardcodear fallbacks de projectId y dataset en sanity.config

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,8 +3,8 @@ import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./src/sanity/schemas";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset   = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "2kodfh48";
+const dataset   = process.env.NEXT_PUBLIC_SANITY_DATASET   ?? "production";
 
 export default defineConfig({
   name: "iiap-studio",
